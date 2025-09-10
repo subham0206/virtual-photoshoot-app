14c29e0db4ad602c60dad956278c868916359f4c
Subham Srivastava <107785834+subham0206@users.noreply.github.com>
2025-09-10 20:49:32 +0530

    Update check_gemini_models.py

--- check_gemini_models.py
+++ check_gemini_models.py
@@ -2,7 +2,7 @@ import os
 import google.generativeai as genai
 
 # Set the API key
-api_key = "AIzaSyB6EWGTcKCrJOLfj_BXcqCn7dAKszQ9X6Y"
+api_key = ""
 genai.configure(api_key=api_key)
 
 # List available models
